37904ca5148e66c85c06ca59af88a06c17c95015
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-26 15:52:31 -0500

    Remove react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { router } from "./App";
+import { App } from "./App";
 import "./index.css";
 import "highlight.js/styles/github-dark.min.css";
-import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
