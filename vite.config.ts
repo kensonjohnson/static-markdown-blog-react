af3460da53fb9e7ff6aea3efe508cb36bcd6d79b
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-26 16:06:17 -0500

    Prepare for deployment to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/static-markdown-blog-react/"
+      : "/",
   plugins: [react(), buildPostsFromMarkdown()],
   assetsInclude: ["**/*.md"],
 });
