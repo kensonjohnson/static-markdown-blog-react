61458bfa4d87832b583b7065c8ecd2b3820e72db
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-26 15:54:06 -0500

    Add github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,12 @@
 export function Footer() {
-  return <footer className="mt-auto">Footer</footer>;
+  return (
+    <footer className="mt-auto flex h-12 items-center justify-center bg-zinc-100 shadow dark:bg-zinc-900">
+      <a
+        href="https://github.com/kensonjohnson/static-markdown-blog-react"
+        className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+      >
+        Github
+      </a>
+    </footer>
+  );
 }
